dab82e58765136b0b514c2498d5cb5f4dc4fe616
LBJ <2197662514@qq.com>
2019-04-10 15:25:19 +0800

    2019.4.10 广告投放系统数据库设计

---
+++ lwy-ad-service/ad-sponsor/src/main/java/lwy/ad/entity/AdPlan.java
@@ -0,0 +1,63 @@
+package lwy.ad.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lwy.ad.constant.CommonStatus;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ad_plan")
+public class AdPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    //构建关联，不在表中使用外键，而在应用程序中使用外键
+    @Basic
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Basic
+    @Column(name = "plan_name", nullable = false)
+    private String planName;
+
+    @Basic
+    @Column(name = "plan_status", nullable = false)
+    private Integer planStatus;
+
+    @Basic
+    @Column(name = "start_date", nullable = false)
+    private Date startDate;
+
+    @Basic
+    @Column(name = "end_date", nullable = false)
+    private Date endDate;
+
+    @Basic
+    @Column(name = "create_time", nullable = false)
+    private Date createTime;
+
+    @Basic
+    @Column(name = "update_time", nullable = false)
+    private Date updateTime;
+
+    public AdPlan(Long userId, String planName,
+                  Date startDate, Date endDate) {
+
+        this.userId = userId;
+        this.planName = planName;
+        this.planStatus = CommonStatus.VALID.getStatus();
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.createTime = new Date();
+        this.updateTime = this.createTime;
+    }
+}
